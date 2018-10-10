a25d48a7632d399741156e249683d3ef41c4b5d3
MadeByKeith <kwarren7677@gmail.com>
2018-10-09 19:03:34 -0700

    JavaScript updates and Typewwriter effect

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,70 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+//Typewriter
+const Typewriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+}
+
+//Type Method
+Typewriter.prototype.type = function () {
+  //Current Index of Word
+  const current = this.wordIndex % this.words.length;
+  //Get Full Text of Current Word
+  const fullTxt = this.words[current];
+
+  //Check if Deleting
+  if (this.isDeleting) {
+    //Remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    //Add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  //Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  //Init Type Speed
+  let typeSpeed = 300;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  //If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    //Pause at end
+    typeSpeed = this.wait;
+    //Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    //Move to next word
+    this.wordIndex++;
+    //Pause before start typing
+    typeSpeed = 500;
+  }
+
+
+  setTimeout(() => this.type(), typeSpeed)
+}
+
+//Init on DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+//Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  //Init Typewriter
+  new Typewriter(txtElement, words, wait);
+}
